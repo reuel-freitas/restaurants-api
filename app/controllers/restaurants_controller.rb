5253782c918a72e86e4ef68e03090c85fd9930da
Reuel Freitas <reuel.cf@outlook.com>
2025-08-28 22:46:53 -0300

    feat: add pagination to restaurants#index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,28 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
-    render json: { restaurants: @restaurants }
+    @restaurants = Restaurant.page(params[:page]).per(10)
+    render json: {
+      restaurants: @restaurants,
+      pagination: {
+        current_page: @restaurants.current_page,
+        total_pages: @restaurants.total_pages,
+        total_count: @restaurants.total_count,
+        per_page: @restaurants.limit_value
+      }
+    }
   end
 
-    def show
-    @restaurant = Restaurant.includes(
-      menus: [
-        :menu_items,
-        :menu_menu_items
-      ]
-    ).find(params[:id])
+  def show
+  @restaurant = Restaurant.includes(
+    menus: [
+      :menu_items,
+      :menu_menu_items
+    ]
+  ).find(params[:id])
 
-    serializer = RestaurantSerializerService.new(@restaurant)
-    render json: serializer.serialize_with_menus
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "Restaurant not found" }, status: :not_found
-    end
+  serializer = RestaurantSerializerService.new(@restaurant)
+  render json: serializer.serialize_with_menus
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Restaurant not found" }, status: :not_found
+  end
 end
